4f679952db375012dcdb91caf74549679f0e35b1
wunluxsze <113801343+wunluxsze@users.noreply.github.com>
2022-11-07 15:19:15 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.add('www')
+// }
+// br()
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.remove('www')
+// }
+// br()
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.contains('www')
+// }
+// br()
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.toggle('www')
+// }
+// br()
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.length
+// }
+// br()
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.classList.length
+//     for (let el of elem.classList) {
+//         alert(el)
+//     }
+// }
+// br()
+
+// function br() {
+//     let elem = document.getElementById('elem');
+//     elem.style.cssText = 'color: red; font-size: 30px; border: 1px solid red';
+// }
+// br()
+
+// function br(){
+//     let elem = document.getElementById('elem');
+//     console.log(elem.tagName);
+// }
+// function br(){
+//     let elem = document.getElementById('elem');
+//     str = elem.tagName;
+//     console.log(str.toLowerCase());
+// }
+// function br() {
+//     let elem = document.querySelectorAll('.www');
+//         for (el of elem){
+//             el.innerHTML = el.innerHTML + el.tagName.toLowerCase()
+//         }
+//     }
+// function br() {
+//     let parent = document.querySelector('#parent');
+//     let p = document.createElement('li');
+//     p.innerHTML = 'пункт';
+//     parent.appendChild(p);
+// }
+// br()
+
+// let ul = document.querySelector('#ul');
+// let names = ['dskjgfds', 'l.gfd', 'lb.gfd'];
+// for (let name = 0; name < names.length; name +=1){
+//     let p = document.createElement('li');
+//     p.innerHTML = names[name];
+//     ul.appendChild(p);
+// }
+
+
+// var d1 = document.getElementById('el');
+// d1.insertAdjacentHTML('beforeend', '<span>!!!</span>');
+
+// var d1 = document.getElementById('el');
+// d1.insertAdjacentHTML('afterbegin', '<span>!!!</span>');
+
+// var d1 = document.getElementById('el');
+// d1.insertAdjacentHTML('beforebegin', '<span>!!!</span>');
+
+// var d1 = document.getElementById('el');
+// d1.insertAdjacentHTML('afterend', '<span>!!!</span>');
+
+//  let remove = document.querySelector('#paret');
+// remove.onclick = function br(){
+
+//     let btn = document.querySelector('#child');
+//     remove.removeChild(btn);
+// }
+
+
+// function br() {
+//     let last = document.querySelector('#awuyd');
+//     last.lastElementChild.remove()
+// }
+
+// function br() {
+//     let last = document.querySelector('#task');
+//     last.remove()
+// }
